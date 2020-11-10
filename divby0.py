e5b7ca25f2b629e98444ac11c93eefc84b5becae
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-10 13:49:10 +0000

    Update divby0.py

--- divby0.py
+++ divby0.py
@@ -1,5 +1,7 @@
+# create class
 class Divby0:
 
+    # verify divisibility by 0 and output True if divisible False if not
     def divby0(self, arg1, arg2):
         if arg1 % arg2 == 0:
             return True
